11b71d418831f8af5d38a03db572641bba99659e
PHEAKTRA.MAO <PHEAKTRA.MAO@pnc.lan>
2023-12-20 09:20:33 +0700

    #10 function with product

--- JS/product.js
+++ JS/product.js
@@ -1,84 +1,154 @@
-let disTable = document.querySelector('.g');
+let display_table = document.querySelector('.g')
 let table = document.querySelector('table');
-let btnAdd = document.querySelector('.add');
-console.log(btnAdd);
-let products = [];
-// disTable.appendChild(table)
+// console.log(table);
+// DOM elements//
+let adddialog = document.querySelector('#add-dialog');
+let ProductsName = document.querySelector("#product-name");
+let discription_name = document.querySelector(".discription");
+let quantity = document.querySelector('#qty');
+let price = document.querySelector('#price');
+let btnAdd = document.querySelector('.Add');
+display_table.appendChild(table)
+// console.log(adddialog);
+// let btnAdd = document.querySelector('.Add');
+// console.log(btnAdd);
+function hide(element) {
+    element.style.display = "none";
+}
 
-// // create new product and add product to product list 
-// let product = {
-//     id: proId,
-//     name: productName.value,
-//     Quantity: Quantity.value,
-//     price: price.value,
-// }
+// Show a given element
+function show(element) {
+    element.style.display = "block";
+}
+
+function saveProducts() {
+    localStorage.setItem("Products", JSON.stringify(Products));
+    localStorage.setItem("Id", JSON.stringify(Unique_Id));
+}
+function getProducts() {
+    // Local Storage ------------------------------------
+    let ProductsStorage = JSON.parse(localStorage.getItem("Products"));
+    let ProductsID = JSON.parse(localStorage.getItem("Id"));
+    if (ProductsStorage != undefined) {
+        Products = ProductsStorage;
+        Unique_Id = ProductsID;
+    } else {
+        saveProducts();
+    }
+    Show_addProducts()
+}
+// Data --------------------------------------
+let Products = [];
+let Unique_Id = 0;
+
+function showDialog() {
+    show(adddialog);
+}
+function add_Products() {
+    console.log(1);
+    Unique_Id = Unique_Id + 1;
+    let Product = {
+        id: Unique_Id,
+        name: ProductsName.value,
+        quantity: quantity.value,
+        price: price.value,
+    }
+    Products.push(Product);
+    // clear input file
+    saveProducts();
+    // getProducts();
+    Show_addProducts()
+    // onCreate()
+    // createRowProducts();    
+    clearInput()
+    ProductsName.value = ""
+    discription_name.value = ""
+    price.value = ""
+    quantity.value = ""
+}
+function clearInput(){
+    ProductsName.value = ""
+    discription_name.value = ""
+}
+function Show_addProducts() {
+    hide(adddialog);
+    let tbody = document.querySelector('tbody');
+    let trs = document.querySelectorAll('tbody tr');
+    for (const tr of trs) {
+        tr.remove();
+    }
+    for (let index = 0; index < Products.length; index++) {
+        // create tr element by using "tableRow"
+        let tableRow = document.createElement('tr')
+        //  create td element by using "tdId"
+        let tdId = document.createElement('td')
+        tdId.textContent = Products[index].id;
+
+        // console.log(tdId);
+        // create td element by usinh "tdName";
+        let tdName = document.createElement('td');
+        tdName.textContent = Products[index].name;
+        
+        // create td element by using "tdQuantity"
+        let tdQuantity = document.createElement('td');
+        tdQuantity.textContent = Products[index].quantity;
+        // create Input element as "qtyInput"
+        const qtyInput = document.createElement('input');
+        
+        // add attribute type input to number
+        qtyInput.setAttribute('type', 'number');
+        // add value to input from product quantity
+        qtyInput.value = Products[index].quantity;
+        // create td element by using "tdPrice"
+        let tdPrice = document.createElement('td');
+        tdPrice.textContent = Products[index].price + "$";
+        // create td element by using "tdAction"
+        let tdAction = document.createElement('td');
+        tdAction.className = "action"
+
+        // create button "edit" and "delete" 
+        let btnDelete = document.createElement('button');
+        btnDelete.setAttribute('class','delete');
+        btnDelete.textContent = "DELETE"
 
 
-// function createProducts() {
-//     // create td element as name tableRow
-//     let tbody = document.querySelector('tbody')
-//     let tableRow = document.createElement('tr');
-//     // createe td element as name tdId
-//     let tdId = document.createElement('td');
-//     tdId.textContent = product.id;
-//     //    create td element as name tdName
-//     let tdName = document.createElement('td');
-//     tdName.textContent = product.Name;
-//     //    crete td element as name tdQuantity
-//     let tdQuantity = document.createElement('td');
-//     tdQuantity.textContent = product.Quantity;
-//     //    create td element as name tdCategories
-//     let tdPrice = document.createElement('td');
-//     tdPrice.textContent = product.price;
-//     let tdCategories = document.createElement('td');
-//     tdCategories.textContent = product.Categories;
-//     //    create td element as name tdPrice
-//     //    create td element as name tdAction
-//     let tdAction = document.createElement('td');
-//     let btnDelete = document.createElement('button')
-//     btnDelete.setAttribute('id', 'delete')
-//     btnDelete.textContent = "Delete"
-//     let btnEdit = document.createElement('button')
-//     btnEdit.setAttribute('id', 'edit')
-//     btnEdit.textContent = "Edit"
-//     let btnView = document.createElement('button')
-//     btnView.setAttribute('id', 'view')
-//     btnView.textContent = "View"
-//     //    append all td to tr
-//     tableRow.appendChild(tdId)
-//     tableRow.appendChild(tdName)
-//     tableRow.appendChild(tdQuantity)
-//     tableRow.appendChild(tdPrice)
-//     tableRow.appendChild(tdCategories)
-//     tableRow.appendChild(tdAction)
-//     tdAction.appendChild(btnDelete)
-//     tdAction.appendChild(btnEdit)
-//     tdAction.appendChild(btnView)
-//     tbody.appendChild(tableRow)
-//     table.appendChild(tbody)
-// }
+        // create button "edit" to edit the row;
+        let btnEdit = document.createElement('button');
+        btnEdit.className = "Edit";
+        btnEdit.textContent = "EDIT"
 
-// // Create function to delete products
-// function deleteProducts() {
-
-// }
-// // create function to edit product 
-// function editProduct() {
-
-// }
-// // Create function to view product
-// function viewProduct() {
-
-// }
-// // Create function to save product
-// function saveProduct() {
-
-// }
-// // create product to load product to localStorage
-// function loadProduct() {
-
-// }
-// // product data
+        // create button "edit" to edit the row;
+        let btnView = document.createElement('button');
+        btnView.className = "View";
+        btnView.textContent = "VIEW"
 
 
+        tdAction.appendChild(btnDelete);
+        tdAction.appendChild(btnEdit);
+        tdAction.appendChild(btnView);
+        // add all td to tbody
 
+        tableRow.appendChild(tdId);
+        tableRow.appendChild(tdName);
+        tableRow.appendChild(tdQuantity);
+        tableRow.appendChild(tdPrice);
+        tableRow.appendChild(tdAction);
+
+        tbody.appendChild(tableRow);
+    }
+    saveProducts();
+    // clearInput();
+    console.log(2);
+    // DeleteList();
+}
+function onCreate() {
+    hide(adddialog);
+    console.log(1);
+
+}
+function onCancel() {
+    adddialog.style.display = "none";
+}
+getProducts();
+btnAdd.addEventListener('click', showDialog);
+// btnAdd.addEventListener('click', showDialog);
